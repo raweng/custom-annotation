2a4ef92e43984a5f34c9dc048fd05cd2baf3bb5e
Akshay Mhatre <akshay.mhatre@raweng.com>
2012-09-03 13:06:22 +0530

    Initial Commit

---
+++ CustomAnnotation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomAnnotation
+//
+//  Created by sobin on 9/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
